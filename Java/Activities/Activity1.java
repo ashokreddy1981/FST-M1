0ae60404526c137d5b5a230b4d7ed7d0984ff958
ashokreddy1981 <105856356+ashokreddy1981@users.noreply.github.com>
2022-06-15 14:14:44 +0530

    Update Activity1.java

--- Java/Activities/Activity1.java
+++ Java/Activities/Activity1.java
@@ -1,8 +1,7 @@
 package activities;
 
 public class Activity1 {
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args) {
         Car obj = new Car();
         System.out.println(obj.name);
     }
